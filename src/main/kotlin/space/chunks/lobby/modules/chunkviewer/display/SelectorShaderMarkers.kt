f0151a5b886e1c9bcaf3b142c30d0584bc33803d
fllipeis <p.eistrach@gmail.com>
2026-08-22 20:12:59 +0200

    fix: keep selected outline at fixed FOV

--- src/main/kotlin/space/chunks/lobby/modules/chunkviewer/display/SelectorShaderMarkers.kt
+++ src/main/kotlin/space/chunks/lobby/modules/chunkviewer/display/SelectorShaderMarkers.kt
@@ -1,5 +1,6 @@
 package space.chunks.lobby.modules.chunkviewer.display
 
+import org.bukkit.Color
 import org.bukkit.entity.Display
 import org.bukkit.entity.ItemDisplay
 import org.bukkit.entity.TextDisplay
@@ -8,6 +9,7 @@ object SelectorShaderMarkers {
     private const val TEXT_OPACITY_MARKER: Byte = -2 // unsigned 254
     private const val ITEM_BLOCK_LIGHT_MARKER = 15
     private const val ITEM_SKY_LIGHT_MARKER = 14
+    private val OUTLINE_COLOR_MARKER = Color.fromRGB(18, 254, 52)
 
     fun mark(display: TextDisplay) {
         display.textOpacity = TEXT_OPACITY_MARKER
@@ -15,5 +17,6 @@ object SelectorShaderMarkers {
 
     fun mark(display: ItemDisplay) {
         display.brightness = Display.Brightness(ITEM_BLOCK_LIGHT_MARKER, ITEM_SKY_LIGHT_MARKER)
+        display.glowColorOverride = OUTLINE_COLOR_MARKER
     }
 }
